55acd1fb8b5eb3a54ad95ad59d9d18f2a2a40ffe
Simone Contorno <83269499+simone-contorno@users.noreply.github.com>
2021-11-25 16:04:05 +0100

    Add files via upload

---
+++ second_assignment_node/src/robot.cpp
@@ -0,0 +1,158 @@
+/* 
+ * Robotics control in a simulated environment
+ * Robotics Engineering - Simone Contorno
+ * 
+ * To see the map type:
+ * rosrun stage_ros stageros $(rospack find second_assignment)/world/my_world.world
+ */
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h" 
+#include "second_assignment_server/Robot.h"
+
+// Publisher
+ros::Publisher pub_vel;
+
+// Service client
+ros::ServiceClient client;
+
+// Global variables
+int flag = 0; // Just to manage the printed strings
+float speed = 0.0; // Robot velocity
+
+/*
+ * 1. Make a request to the server for the actual robot velocity
+ * 2. Take laser values and check for the obstacles
+ * 3. Publish robot velocity
+ */ 
+void robotCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    // Local variables
+    float first_value = 0;
+    float mid_value = 30;
+    float last_value = 0;
+
+    // 1. Make the request to the server 
+    second_assignment_server::Robot com;
+    com.request.command = 4.0;
+    client.waitForExistence();
+    client.call(com);
+
+    if (speed != com.response.speed) {
+        speed = com.response.speed;
+        printf("Actual robot velocity: %f\n", speed);
+    }
+
+    // 2. Take the minimum value on the left
+    int i = 0;
+    while (i < 120) {
+        if (msg->ranges[i] > first_value)
+            first_value = msg->ranges[i];
+        i++;
+    }
+
+    // Take the minimum value in the middle
+    i = 300;
+    while (i < 420) {
+        if (msg->ranges[i] < mid_value)
+            mid_value = msg->ranges[i];
+        i++;
+    }
+
+    // Take the minimum value on the right
+    i = 600;
+    while (i <= 720) {
+        if (msg->ranges[i] > last_value)
+            last_value = msg->ranges[i];
+        i++;
+    }
+
+    /* 
+     * Compute the differenze between the minimum value
+     * on the right and on the left to decide in which 
+     * direction turn when an obstacle is too close to the robot
+     */ 
+    float diff = last_value - first_value;
+    float abs_diff = diff;
+    if (abs_diff < 0) 
+        abs_diff *= -1;
+    if (abs_diff > 1) {
+        abs_diff = 1;
+    }
+    else if (abs_diff < -1) {
+        abs_diff = -1;
+    }
+
+    // Set robot velocity
+	geometry_msgs::Twist robot_vel;
+    
+    /* 
+     * If the obstacle is too close use the diff value computed above
+     * to choose in which direction turn the robot
+     */ 
+    if (mid_value < 1) {
+        if (flag == 1) 
+            printf("I'm too close to an obstacle!\n");
+        
+        // If the robot has more space on the right
+        if (diff < 0) {
+            printf("Turn Right...\n\n");
+            if (diff < -1) {
+                robot_vel.linear.x = 0;
+                robot_vel.angular.z = diff; 
+            }
+            else {
+                robot_vel.angular.z = -1;
+                robot_vel.linear.x = abs_diff;
+            }
+        }
+
+        // If the robot has more space on the left
+        else if (diff > 0) {
+            printf("Turn Left...\n\n");
+            if (diff > 1) {
+                robot_vel.linear.x = 0; 
+                robot_vel.angular.z = diff; 
+            }
+            else {
+                robot_vel.angular.z = 1;
+                robot_vel.linear.x = abs_diff;
+            }     
+        }
+        flag = 0;
+    }
+
+    // Go on 
+    else {
+        if (flag == 0) {
+            printf("Go on...\n\n");
+            flag = 1;
+        }
+        if (speed * (mid_value / 3) >= speed)
+            robot_vel.linear.x = speed;
+        else 
+            robot_vel.linear.x = speed * (mid_value / 3);
+        robot_vel.angular.z = 0.0;
+    }
+   
+    // Publish robot velocity
+	pub_vel.publish(robot_vel);
+}
+
+int main(int argc, char **argv) {
+    // Initialize the node
+    ros::init(argc, argv, "robot");
+    ros::NodeHandle nh;
+    
+    // Publisher velocity
+    pub_vel = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+
+    // Client
+    client = nh.serviceClient<second_assignment_server::Robot>("/robot_control");
+
+    // Subscriber laser info
+    ros::Subscriber sub_las = nh.subscribe("/base_scan", 1000, robotCallback);
+
+    ros::spin();
+    return 0;
+}
---
+++ second_assignment_node/src/robot_controller.cpp
@@ -0,0 +1,99 @@
+/* 
+ * Robotics control in a simulated environment
+ * Robotics Engineering - Simone Contorno
+ * 
+ * To see the map type:
+ * rosrun stage_ros stageros $(rospack find second_assignment)/world/my_world.world
+ */
+
+#include "ros/ros.h"
+#include "std_srvs/Empty.h"
+#include "second_assignment_server/Robot.h"
+
+// Publisher
+ros::Publisher pub_vel;
+
+// Service clients
+ros::ServiceClient client1;
+ros::ServiceClient client2;
+
+// Global variable
+float speed = 0.0; // Robot velocity
+
+int main(int argc, char **argv) {
+    // Initialize the node
+    ros::init(argc, argv, "robot_controller");
+    ros::NodeHandle rc;
+    
+    // Clients
+    client1 = rc.serviceClient<second_assignment_server::Robot>("/robot_control");
+    client2 = rc.serviceClient<std_srvs::Empty>("/reset_positions");
+    
+    int in = -1; // User's input
+
+    // Show list of commands 
+    printf("Type a command:\n\n"
+        "0 - Reset robot position\n"
+        "1 - Increase robot velocity\n"
+        "2 - Decrease robot velocity\n"
+        "3 - Stop the robot\n"
+        "4 - Exit\n\n"
+        "-------------------------------\n\n"
+    );
+
+    while (true) {
+        in = -1;
+
+        // Take input value
+        printf("Command: ");
+        std::cin >> in;
+
+        // Reset
+        if (in == 0) {
+            std_srvs::Empty res;
+            client2.waitForExistence();
+            client2.call(res);
+            printf("Robot position reset.\n");
+        }
+
+        // Manage robot's velocity
+        else if (in == 1 || in == 2 || in == 3) {
+            // Command
+            second_assignment_server::Robot com;
+
+            // Increase velocity
+            if (in == 1) {
+                printf("Increasing velocity...");
+                com.request.command = 1.0;
+                com.request.speed = speed;
+            }
+            // Decrease velocity
+            else if (in == 2) {
+                printf("Decreasing velocity...");
+                com.request.command = 2.0;
+                com.request.speed = speed;
+            }
+            // Stop the robot
+            else {
+                printf("Stopping the robot... ");
+                com.request.command = 3.0;
+                com.request.speed = 0;
+            }
+
+            // Send the request to the server and wait for the response
+            client1.waitForExistence();
+            client1.call(com);
+            speed = com.response.speed;
+            printf("New speed: %f\n", speed);
+        }
+        // Exit
+        else if (in == 4) {
+            exit(0);
+        }
+        // Manage wrong inputs
+        else {
+            printf("ERROR! Command not valid: type '0', '1', '2', '3' or '4'.\n");
+        }
+    }
+    return 0;
+}
